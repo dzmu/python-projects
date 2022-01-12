49be61ba3bae8e49e53352d04d68d9a271f58758
dzmu <88698731+dzmu@users.noreply.github.com>
2022-01-12 13:24:55 -0500

    Add files via upload

---
+++ mad_libs.py
@@ -0,0 +1,22 @@
+# Author: Tanner Mesaric
+Article_of_Clothing = input("Enter Article of Clothing:")
+Adjective = input("Enter Adjective:")
+Plural_Noun = input("Enter Plural Noun:")
+Sport = input("Enter Sport:")
+Noun = input("Enter Noun:")
+Adjective2 = input("Enter Adjective:")
+Adverb = input("Enter Adverb:")
+Proper_Noun = input("Enter Proper Noun:")
+Song_Title = input("Enter Song Title:")
+Interjection = input("Enter Interjection:")
+Clothing = input("Enter Article of Clothing:")
+
+print (f"""
+I scoured my closet for the perfect {Article_of_Clothing} to wear. Dressing for the most {Adjective} place
+around isn’t easy. Sweating {Plural_Noun} during the interview is a sure sign of weakness. I must
+remember to play up my strengths: {Sport}, menacing stares, and {Noun} collecting.
+It has always been my dream to work with the Angel of Coding, or {Adjective2} Developer. The power
+and prestige are {Adverb} desirable.
+The Gates of {Proper_Noun} tower above me as I ring the doorbell. I can’t believe {Song_Title} is
+the doorbell chime. {Interjection}, his unicorn {Clothing} looks better in person!
+""")
